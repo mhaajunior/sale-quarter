786b5b567b789c4d274ef5bc9e363eaad9b67f0a
mhaajunior <junior29958@gmail.com>
2024-02-15 13:26:21 +0700

    config env settings

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+new webpack.DefinePlugin({
+  "process.env.DATABASE_URL": JSON.stringify(process.env.DATABASE_URL),
+  "process.env.JWT_SECRET": JSON.stringify(process.env.JWT_SECRET),
+  "process.env.NEXTAUTH_SECRET": JSON.stringify(process.env.NEXTAUTH_SECRET),
+  "process.env.NEXTAUTH_URL": JSON.stringify(process.env.NEXTAUTH_URL),
+  "process.env.NEXT_PUBLIC_CALLBACK_URL": JSON.stringify(
+    process.env.NEXT_PUBLIC_CALLBACK_URL
+  ),
+  "process.env.JWT_SECRET": JSON.stringify(process.env.JWT_SECRET),
+});
